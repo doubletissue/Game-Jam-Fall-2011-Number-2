825a4b33fb882fa3ba8a5d155d584b4034de9c04
Michael <michorowitz@gmail.com>
2011-10-22 19:57:54 -0400

    Dan is a jerk

--- player.py
+++ player.py
@@ -1,2 +1,11 @@
-class Player:
-  pass
+from __future__ import division
+
+class Player(object):
+  def __init__( self, (x, y) ):
+    self.x, self.y = x, y
+
+  def update_controls( self, player_input ):
+    pass
+
+  def update_physics( self, collidables ):
+    [(),()]
