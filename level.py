8903789df20511ebd0062ca4510bc6fc48fad6f3
Christopher Thomas <cmthomas@cmthomas.us>
2011-10-23 00:43:39 -0400

    new and improved -- now with proper bounds checking

--- level.py
+++ level.py
@@ -95,10 +95,14 @@ class level(object):
 
   def collision(self, x, y):
     start_x = (int(x) // 4) - 1
+    if start_x < 0: start_x = 0
     start_y = (int(y) // 4) - 1
+    if start_y < 0: start_y = 0
     obj_list = []
     for i in range(0, 4):
       for j in range(0, 4):
+        if start_x + i > 127 or start_y + j > 127:
+          continue
         if self._obstacles[start_x + i][start_y + j]:
           collision_x = (start_x + i) * 4
           collision_y = (start_y + j) * 4
