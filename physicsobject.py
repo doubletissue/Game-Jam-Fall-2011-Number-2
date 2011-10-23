80fcc405ba2eb1400eda5da2e42989a71f6117a3
Michael <michorowitz@gmail.com>
2011-10-23 00:18:44 -0400

    Updated

--- physicsobject.py
+++ physicsobject.py
@@ -98,7 +98,7 @@ class PhysicsObject(object):
   def intersect( self, other ):
     print self.isAbove( other, True ), self.isBelow( other, True ), self.isLeft( other, True ), self.isRight( other, True )
     if not self.isAbove( other, True ) and not self.isBelow( other, True ) and not self.isLeft( other, True ) and not self.isRight( other, True ):
-      print "top"
+      print "Collision"
       if self.isAbove( other ):
         print "top"
         return "top"
