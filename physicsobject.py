2862cdaa20266bf9a59eeae8f0662184de04c6ee
Michael <michorowitz@gmail.com>
2011-10-23 01:15:08 -0400

    Updated

--- physicsobject.py
+++ physicsobject.py
@@ -63,16 +63,16 @@ class PhysicsObject(object):
     self.next_y = self.y
 
     if intersection == "top":
-      #self.next_y = other.y + 16
+      self.next_y = other.y + 16
       selfdy = -(selfdy + otherdy)/2
     elif intersection == "bottom":
-      #self.next_y = other.y - 16
+      self.next_y = other.y - 16
       selfdy = (selfdy + otherdy)/2
     elif intersection == "left":
-      #self.next_x = other.x - 16
+      self.next_x = other.x - 16
       selfdx = -(selfdx + otherdx)/2
     elif intersection == "right":
-      #self.next_x = other.x + 16
+      self.next_x = other.x + 16
       selfdx = -(selfdx + otherdx)/2
 
     self.next_vmag, self.next_varg = rect_to_polar( selfdx, selfdy )
