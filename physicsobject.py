af1a0153672bc1e5a6f24f6237deace9ae1be551
Michael <michorowitz@gmail.com>
2011-10-22 20:30:13 -0400

    updated

--- physicsobject.py
+++ physicsobject.py
@@ -1,16 +1,21 @@
 from __future__ import division
 import math
 
+MAX_VELOCITY = 25
+
 class PhysicsObject(object):
-  def __init__( self, (x,y), (dx,dy), (sizex,sizey) )
+  def __init__( self, (x,y), (dx,dy), (sizex,sizey) ):
     self.x, self.y = x,y
     self.dx, self.dy = dx,dy
     self.sizex, self.sizey = sizex, sizey
 
   def update( self, a, dr ):
     mag = abs(complex(self.x,self.y))
-    arg = math.tan2( y, x )
-    
+    arg = math.tan2( self.y, self.x )
+
+    arg = (arg + dr)
+
+    abs(complex(self.dx,self.dy))
 
   def isAbove( self, other ):
     return self.y < other.y
@@ -25,7 +30,7 @@ class PhysicsObject(object):
     return self.x > other.x
 
   def intersect( self, other ):
-    
+    pass
 
   def bounce( self, other ):
     pass
