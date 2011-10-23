a789d7ce9faa4829bab83e155ecabef286f24c5b
Michael <michorowitz@gmail.com>
2011-10-22 20:43:33 -0400

    Physics object update

--- physicsobject.py
+++ physicsobject.py
@@ -9,13 +9,28 @@ class PhysicsObject(object):
     self.dx, self.dy = dx,dy
     self.sizex, self.sizey = sizex, sizey
 
+  def __str__( self ):
+    return "x: {}, y: {}; dx: {}, dy: {}; sizex: {}, sizey: {}".format( self.x, self.y, self.dx, self.dy, self.sizex, self.sizey )
+
   def update( self, a, dr ):
     mag = abs(complex(self.x,self.y))
-    arg = math.tan2( self.y, self.x )
+    arg = math.atan2( self.y, self.x )
+    if self.x < 0:
+      arg = - arg
 
-    arg = (arg + dr)
+    #Update the angle
+    arg = (arg + dr) % 2*math.pi
 
-    abs(complex(self.dx,self.dy))
+    #Update the magnitude
+    mag += a
+    if mag > MAX_VELOCITY:
+      mag = MAX_VELOCITY
+
+    self.dx = mag*math.cos(arg)
+    self.dy = mag*math.sin(arg)
+
+    self.x += self.dx
+    self.y += self.dy
 
   def isAbove( self, other ):
     return self.y < other.y
@@ -34,3 +49,14 @@ class PhysicsObject(object):
 
   def bounce( self, other ):
     pass
+
+if __name__ == "__main__":
+  a = PhysicsObject( (0,0), (0,0), (0,0) )
+  print a
+  a.update( 0, 0 )
+  print a
+  a.update( 1, 0  )
+  print a
+  a.update( 0, math.pi )
+  print a
+  a.update( 1, math.pi )
