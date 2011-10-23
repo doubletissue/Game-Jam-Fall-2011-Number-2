e46214e6cb1acae06be1e1583e463265e2260d4c
Michael <michorowitz@gmail.com>
2011-10-22 20:05:26 -0400

    Added physicsobject

---
+++ physicsobject.py
@@ -0,0 +1,5 @@
+class PhysicsObject(object):
+  def __init__( self, (x,y), (dx,dy), (sizex,sizey) )
+    self.x, self.y = x,y
+    self.dx, self.dy = dx,dy
+    self.sizex, self.sizey = sizex, sizey
